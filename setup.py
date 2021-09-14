afb6d8c29771e94a5dceac2814d19ee08c341c26
zolekode <pascalzoleko@gmail.com>
2021-09-15 00:47:31 +0200

    updated setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='natural-language-logic',
-    version='0.0.1',
+    version='0.0.3',
     packages=['natural-language-logic'],
     url='https://flexudy.com',
     license='',
