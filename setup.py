bf1cc544e78b52cd7f32c0c73a78be602b886603
zolekode <pascalzoleko@gmail.com>
2021-09-15 00:45:53 +0200

    updated setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from distutils.core import setup
-import setuptools
 
 setup(
     name='natural-language-logic',
     version='0.0.1',
-    packages=setuptools.find_packages(),
+    packages=['natural-language-logic'],
     url='https://flexudy.com',
     license='',
     author='Flexudy',
     author_email='support@flexudy.com',
-    description='Natural Language Logic'
+    description='Natural Language Logic',
+    install_requires=["pytest==6.2.1",
+                      "pytest-cov==2.10.1",
+                      "numpy~=1.19.4",
+                      "torch==1.9.0",
+                      "transformers==4.7.0",
+                      "sentencepiece==0.1.95",
+                      "protobuf==3.15.3",
+                      "langdetect==1.0.9"]
 )
