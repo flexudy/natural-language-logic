07dcb761fd68bf1ae1744453b9dfa8afc4a1eb47
zolekode <pascalzoleko@gmail.com>
2021-09-15 00:29:51 +0200

    setup files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+    name='natural-language-logic',
+    version='0.0.1',
+    packages=setuptools.find_packages(),
+    url='https://flexudy.com',
+    license='',
+    author='Flexudy',
+    author_email='support@flexudy.com',
+    description='Natural Language Logic'
+)
